0270632ba2799b4c1023c817cbf641ada73a7272
Moredespicablethananyoneelse <donotblieveit@outlook.com>
2024-12-19 12:24:57 +0800

    Writing your first Django ap, part 3

--- polls/views.py
+++ polls/views.py
@@ -8,13 +8,25 @@ from django.shortcuts import render
 #    return HttpResponse("Hello, world. You're at the polls index.")
 
 # coded by me Wring your first Django app, part 3
+#from .models import Question
+#from django.http import HttpResponse
+#def index(request):
+#    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#    output = ", ".join([q.question_text for q in latest_question_list])
+#    return HttpResponse(output)
+
+#coded by me Writing your first Django app, part3
 from .models import Question
 from django.http import HttpResponse
+from django.template import loader
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list" : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 # coded by me Writing your first Django app, part 1
